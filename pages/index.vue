318339b9f5506bd4fd2eafb89da2bddd39feaf5f
Gregory COLOMBE <colombe@abvent.fr>
2021-06-15 12:19:23 +0200

    test

--- pages/index.vue
+++ pages/index.vue
@@ -52,7 +52,7 @@ export default {
     }),
     computed: {
         ...mapGetters({
-            resellers: "map/getResellers"
+            resellers: "map/getResellers" 
         })
     },
     methods: {
