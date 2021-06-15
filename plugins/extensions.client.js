a6e5d757569277ba187771b6fc7673c736c99a1d
Gregory COLOMBE <colombe@abvent.fr>
2021-06-15 16:33:44 +0200

    [Update] clean code

--- plugins/extensions.client.js
+++ plugins/extensions.client.js
@@ -1,5 +1,5 @@
 import Vue from "vue"
 import { TweenMax, Power4 } from "gsap"
 
-Vue.prototype.$TweenMax     = TweenMax
-Vue.prototype.$Ease         = Power4
+Vue.prototype.$TweenMax = TweenMax
+Vue.prototype.$Ease     = Power4
